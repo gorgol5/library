bc9c3e80d074d1ff4806d4958a3998b4135e6e14
Michal Gorgolewski <gorgolewskimichal@gmail.com>
2020-08-21 21:30:18 +0200

    v 0.2

--- main.js
+++ main.js
@@ -1,22 +1,56 @@
 let myLibrary = [];
-function Book(title, author, pages, read) {
+function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.status = status;
   this.info = function () {
-    return `${title} by ${author}, ${pages} pages, ${read}`;
+    return `${title} by ${author}, ${pages} pages, ${status}`;
   };
 }
 let takeData = function () {
-  let title = prompt("Please enter title of your book", "Harry Potter");
-  let author = prompt("Please enter author of your book", "Harry Potter");
-  let pages = prompt("Please enter pages of your book", "Harry Potter");
-  let read = prompt("Did you read the book", "Harry Potter");
-  book1 = new Book(title, author, pages, read);
+  let title = document.querySelector(".input-title").value;
+  let author = document.querySelector(".input-author").value;
+  let pages = document.querySelector(".input-pages").value;
+  let status = document.querySelector(".input-status").value;
+  book1 = new Book(title, author, pages, status);
   myLibrary.push(book1);
 };
-// console.log(book1.info());
+let addForm = function () {
+  let form = document.querySelector(".form");
+  let input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("name", "book-title");
+  input.setAttribute("class", "input-title");
+  input.setAttribute("placeholder", "title of your book");
+  form.appendChild(input);
+  input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("name", "book-author");
+  input.setAttribute("class", "input-author");
+  input.setAttribute("placeholder", "author of your book");
+  form.appendChild(input);
+  input = document.createElement("input");
+  input.setAttribute("type", "number");
+  input.setAttribute("name", "book-pages");
+  input.setAttribute("class", "input-pages");
+  input.setAttribute("placeholder", "number of pages in your book");
+  form.appendChild(input);
+  input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("name", "book-status");
+  input.setAttribute("class", "input-status");
+  input.setAttribute("placeholder", "did you read the book?");
+  form.appendChild(input);
+  var s = document.createElement("input");
+  s.setAttribute("type", "submit");
+  s.setAttribute("value", "Submit");
+  s.setAttribute("class", "input-submit");
+  form.appendChild(s);
+  let inputSubmit = document.querySelector(".input-submit");
+  inputSubmit.addEventListener("click", addBookToLibrary);
+};
+
 let book1 = {};
 let timesClicked = 0;
 let addBookToLibrary = function () {
@@ -28,15 +62,15 @@ let addBookToLibrary = function () {
     render(author, document.querySelector(".book-author"));
     pages = `${myLibrary[i].pages}`;
     render(pages, document.querySelector(".book-pages"));
-    read = `${myLibrary[i].read}`;
-    render(read, document.querySelector(".book-status"));
+    status = `${myLibrary[i].status}`;
+    render(status, document.querySelector(".book-status"));
     render("", document.querySelector(".book-remove"));
   }
   timesClicked++;
   console.log(timesClicked);
 };
 let button = document.querySelector(".add-book");
-button.addEventListener("click", addBookToLibrary);
+button.addEventListener("click", addForm);
 let table = document.querySelector(".table");
 let div = document.createElement("div");
 
